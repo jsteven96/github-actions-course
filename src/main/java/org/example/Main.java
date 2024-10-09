3b6a1b30aaa3654213693689cab5cabac807b4dd
javierPerezSB <javier.perez@segurosbolivar.com>
2024-10-08 21:21:28 -0500

    Ajuste de código

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!" + args[0] ? args[0] : "");
+        String saludo = "Hello world!";
+        if (args.length > 0) {
+            saludo += args[0];
+        }
+        System.out.println(saludo);
     }
 }
